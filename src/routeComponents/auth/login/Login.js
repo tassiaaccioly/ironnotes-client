78f1846fb77676fba57b31415bef8496aceb8380
Tassia Accioly <tassia.accioly@gmail.com>
2020-12-14 18:29:24 -0300

    temporary

--- src/routeComponents/auth/login/Login.js
+++ src/routeComponents/auth/login/Login.js
@@ -3,13 +3,19 @@ import LoginForm from "../login/LoginForm";
 import Logo from "../../../assets/images/LogoLight.svg";
 import "./Login.css";
 
-function Login() {
+function Login(props) {
   return (
     <div className="login">
-      <img className="Logo" type="image/svg+xml" style={{width: '250px'}} src={Logo} alt="Logo" />
+      <img
+        className="Logo"
+        type="image/svg+xml"
+        style={{ width: "250px" }}
+        src={Logo}
+        alt="Logo"
+      />
       <div>
-      <LoginForm />
-        </div>
+        <LoginForm history={props.history} />
+      </div>
     </div>
   );
 }
